5d03674e01cfe4742a1a93302a7c5067c9da086e
Issei Kumagai <Kumagaiissei@hotmail.com>
2022-07-29 17:41:34 +0800

    commit second

--- src/restaking-farm.ts
+++ src/restaking-farm.ts
@@ -1,95 +1,342 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  RestakingFarm,
+  BigInt,
+  BigDecimal,
+  store,
+  Address,
+  log,
+} from "@graphprotocol/graph-ts";
+
+import {
   AddNewPool,
-  AdminChanged,
-  BeaconUpgraded,
   ClaimReward,
   Deposit,
   EmergencyWithdraw,
-  OwnershipTransferred,
-  Paused,
-  Unpaused,
   UpdatePoolMultiplier,
   UpdatePoolReward,
-  Upgraded,
-  Withdraw
-} from "../generated/RestakingFarm/RestakingFarm"
-import { ExampleEntity } from "../generated/schema"
+  Withdraw,
+  RestakingFarm,
+} from "../generated/RestakingFarmSubgraph/RestakingFarm"; //These are all events + contract from the smart contracts
 
-export function handleAddNewPool(event: AddNewPool): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+import {
+  UserActions,
+  UserDeposit,
+  UserWithdraw,
+  UserAddNewPool,
+  UserUpdatePoolReward,
+  UserUpdatePoolMultiplier,
+  UserClaimReward,
+  UserEmergencyWithdraw,
+} from "../generated/schema";
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+// make this work with the below functions
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+// event Deposit(address indexed user, uint256 amount);
+// event Withdraw(address indexed user, uint256 amount);
+// event AddNewPool(address indexed owner, IERC20Upgradeable indexed lpToken, uint256 pursePerBlock, uint256 bonusMultiplier, uint256 startBlock);
+// event UpdatePoolReward(address indexed owner, IERC20Upgradeable indexed lpToken, uint256 pursePerBlock);
+// event UpdatePoolMultiplier(address indexed owner, IERC20Upgradeable indexed lpToken, uint256 bonusMultiplier);
+// event ClaimReward(address indexed user, uint256 amount);
+// event EmergencyWithdraw(address indexed user, IERC20Upgradeable indexed lpToken, uint256 amount);
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.lpToken = event.params.lpToken
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.capMintToken(...)
-  // - contract.getMultiplier(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.paused(...)
-  // - contract.pendingReward(...)
-  // - contract.poolInfo(...)
-  // - contract.poolLength(...)
-  // - contract.poolTokenList(...)
-  // - contract.purseToken(...)
-  // - contract.totalMintToken(...)
-  // - contract.userInfo(...)
+function createUserActions(address: Address): UserActions {
+  let entityUserActions = new UserActions(address.toHexString());
+  entityUserActions.userDeposits = [];
+  entityUserActions.userWithdraws = [];
+  entityUserActions.userAddNewPools = [];
+  entityUserActions.userUpdatePoolRewards = [];
+  entityUserActions.userUpdatePoolMultipliers = [];
+  entityUserActions.userClaimRewards = [];
+  entityUserActions.userEmergencyWithdraws = [];
+  entityUserActions.save();
+  return entityUserActions as UserActions;
 }
 
-export function handleAdminChanged(event: AdminChanged): void {}
+export function handleAddNewPool(event: AddNewPool): void {
+  // load UserActions entity
+  let entityUserActions = UserActions.load(
+    event.transaction.from.toHexString()
+  );
 
-export function handleBeaconUpgraded(event: BeaconUpgraded): void {}
+  // if UserActions entiry for this transaction caller does not exist, create new.
+  if (entityUserActions === null) {
+    entityUserActions = createUserActions(event.transaction.from);
+  }
 
-export function handleClaimReward(event: ClaimReward): void {}
+  // create new entity for the one in question
+  let userAddNewPools = entityUserActions.userAddNewPools;
+  let entityUserAddNewPool = new UserAddNewPool(
+    event.transaction.hash
+      .toHexString()
+      .concat("-")
+      .concat(BigInt.fromI32(userAddNewPools.length).toString())
+  );
 
-export function handleDeposit(event: Deposit): void {}
+  // who called the function ?
+  entityUserAddNewPool.sender = event.params.owner; // straight from the event
+  entityUserAddNewPool.from = event.transaction.from;
 
-export function handleEmergencyWithdraw(event: EmergencyWithdraw): void {}
+  // straight from the events
+  entityUserAddNewPool.lpTokenPoolAddress = event.params.lpToken;
+  entityUserAddNewPool.amountTokenPerBlock = event.params.pursePerBlock;
+  entityUserAddNewPool.numberBonusMultiplier = event.params.bonusMultiplier;
+  entityUserAddNewPool.numberStartBlock = event.params.startBlock;
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+  // For historical data retreival we need timestamp and block number
+  entityUserAddNewPool.timestamp = event.block.timestamp;
+  entityUserAddNewPool.blockNumber = event.block.number;
 
-export function handlePaused(event: Paused): void {}
+  entityUserActions.userAddNewPools = userAddNewPools.concat([
+    entityUserAddNewPool.id,
+  ]);
+  // userAddNewPools?.push(entityUserAddNewPool.id);
+  // entityUserActions.userAddNewPools = userAddNewPools;
+  entityUserAddNewPool.save();
+  entityUserActions.save();
+}
 
-export function handleUnpaused(event: Unpaused): void {}
+export function handleDeposit(event: Deposit): void {
+  // load UserActions entity
+  let entityUserActions = UserActions.load(
+    event.transaction.from.toHexString()
+  );
 
-export function handleUpdatePoolMultiplier(event: UpdatePoolMultiplier): void {}
+  // if UserActions entiry for this transaction caller does not exist, create new.
+  if (entityUserActions === null) {
+    entityUserActions = createUserActions(event.transaction.from);
+  }
 
-export function handleUpdatePoolReward(event: UpdatePoolReward): void {}
+  // create new entity for the one in question
+  let userDeposits = entityUserActions.userDeposits;
+  let entityUserDeposit = new UserDeposit(
+    event.transaction.hash
+      .toHexString()
+      .concat("-")
+      .concat(BigInt.fromI32(userDeposits.length).toString())
+  );
 
-export function handleUpgraded(event: Upgraded): void {}
+  // let restakingFarmContract = RestakingFarm.bind(event.address); // need to access the contract for local variable
+  // entityUserDeposit.lpTokenPoolAddress = restakingFarmContract.
 
-export function handleWithdraw(event: Withdraw): void {}
+  // who called the function ?
+  entityUserDeposit.sender = event.params.user; // straight from the event
+  entityUserDeposit.from = event.transaction.from;
+
+  // straight from the events
+  entityUserDeposit.amountDeposit = event.params.amount;
+
+  // For historical data retreival we need timestamp and block number
+  entityUserDeposit.timestamp = event.block.timestamp;
+  entityUserDeposit.blockNumber = event.block.number;
+
+  entityUserActions.userDeposits = userDeposits.concat([entityUserDeposit.id]);
+  // userDeposits?.push(entityUserDeposit.id);
+  // entityUserActions.userDeposits = userDeposits;
+  entityUserDeposit.save();
+  entityUserActions.save();
+}
+
+export function handleClaimReward(event: ClaimReward): void {
+  // load UserActions entity
+  let entityUserActions = UserActions.load(
+    event.transaction.from.toHexString()
+  );
+
+  // if UserActions entiry for this transaction caller does not exist, create new.
+  if (entityUserActions === null) {
+    entityUserActions = createUserActions(event.transaction.from);
+  }
+
+  // create new entity for the one in question
+  let userClaimRewards = entityUserActions.userClaimRewards;
+  let entityUserClaimReward = new UserClaimReward(
+    event.transaction.hash
+      .toHexString()
+      .concat("-")
+      .concat(BigInt.fromI32(userClaimRewards.length).toString())
+  );
+
+  // who called the function ?
+  entityUserClaimReward.sender = event.params.user; // straight from the event
+  entityUserClaimReward.from = event.transaction.from;
+
+  // straight from the events
+  entityUserClaimReward.pendingAmount = event.params.amount;
+
+  // For historical data retreival we need timestamp and block number
+  entityUserClaimReward.timestamp = event.block.timestamp;
+  entityUserClaimReward.blockNumber = event.block.number;
+
+  entityUserActions.userClaimRewards = userClaimRewards.concat([
+    entityUserClaimReward.id,
+  ]);
+  // userClaimRewards?.push(entityUserClaimReward.id);
+  // entityUserActions.userClaimRewards = userClaimRewards;
+  entityUserClaimReward.save();
+  entityUserActions.save();
+}
+
+export function handleEmergencyWithdraw(event: EmergencyWithdraw): void {
+  // load UserActions entity
+  let entityUserActions = UserActions.load(
+    event.transaction.from.toHexString()
+  );
+
+  // if UserActions entiry for this transaction caller does not exist, create new.
+  if (entityUserActions === null) {
+    entityUserActions = createUserActions(event.transaction.from);
+  }
+
+  // create new entity for the one in question
+  let userEmergencyWithdraws = entityUserActions.userEmergencyWithdraws;
+  let entityUserEmergencyWithdraw = new UserEmergencyWithdraw(
+    event.transaction.hash
+      .toHexString()
+      .concat("-")
+      .concat(BigInt.fromI32(userEmergencyWithdraws.length).toString())
+  );
+
+  // who called the function ?
+  entityUserEmergencyWithdraw.sender = event.params.user; // straight from the event
+  entityUserEmergencyWithdraw.from = event.transaction.from;
+
+  // straight from the events
+  entityUserEmergencyWithdraw.lpTokenPoolAddress = event.params.lpToken;
+  entityUserEmergencyWithdraw.totalAmountWithdraw = event.params.amount;
+
+  // For historical data retreival we need timestamp and block number
+  entityUserEmergencyWithdraw.timestamp = event.block.timestamp;
+  entityUserEmergencyWithdraw.blockNumber = event.block.number;
+
+  entityUserActions.userEmergencyWithdraws = userEmergencyWithdraws.concat([
+    entityUserEmergencyWithdraw.id,
+  ]);
+  // userEmergencyWithdraws?.push(entityUserEmergencyWithdraw.id);
+  // entityUserActions.userEmergencyWithdraws = userEmergencyWithdraws;
+  entityUserEmergencyWithdraw.save();
+  entityUserActions.save();
+}
+
+export function handleUpdatePoolMultiplier(event: UpdatePoolMultiplier): void {
+  // load UserActions entity
+  let entityUserActions = UserActions.load(
+    event.transaction.from.toHexString()
+  );
+
+  // if UserActions entiry for this transaction caller does not exist, create new.
+  if (entityUserActions === null) {
+    entityUserActions = createUserActions(event.transaction.from);
+  }
+
+  // create new entity for the one in question
+  let userUpdatePoolMultipliers = entityUserActions.userUpdatePoolMultipliers;
+  let entityUserUpdatePoolMultiplier = new UserUpdatePoolMultiplier(
+    event.transaction.hash
+      .toHexString()
+      .concat("-")
+      .concat(BigInt.fromI32(userUpdatePoolMultipliers.length).toString())
+  );
+
+  // who called the function ?
+  entityUserUpdatePoolMultiplier.sender = event.params.owner; // straight from the event
+  entityUserUpdatePoolMultiplier.from = event.transaction.from;
+
+  // straight from the events
+  entityUserUpdatePoolMultiplier.lpTokenPoolAddress = event.params.lpToken;
+  entityUserUpdatePoolMultiplier.numberBonusMultiplier =
+    event.params.bonusMultiplier;
+
+  // For historical data retreival we need timestamp and block number
+  entityUserUpdatePoolMultiplier.timestamp = event.block.timestamp;
+  entityUserUpdatePoolMultiplier.blockNumber = event.block.number;
+
+  entityUserActions.userUpdatePoolMultipliers = userUpdatePoolMultipliers.concat(
+    [entityUserUpdatePoolMultiplier.id]
+  );
+  // userUpdatePoolMultipliers?.push(entityUserUpdatePoolMultiplier.id);
+  // entityUserActions.userUpdatePoolMultipliers = userUpdatePoolMultipliers;
+  entityUserUpdatePoolMultiplier.save();
+  entityUserActions.save();
+}
+
+export function handleUpdatePoolReward(event: UpdatePoolReward): void {
+  // load UserActions entity
+  let entityUserActions = UserActions.load(
+    event.transaction.from.toHexString()
+  );
+
+  // if UserActions entiry for this transaction caller does not exist, create new.
+  if (entityUserActions === null) {
+    entityUserActions = createUserActions(event.transaction.from);
+  }
+
+  // create new entity for the one in question
+  let userUpdatePoolRewards = entityUserActions.userUpdatePoolRewards;
+  let entityUserUpdatePoolReward = new UserUpdatePoolReward(
+    event.transaction.hash
+      .toHexString()
+      .concat("-")
+      .concat(BigInt.fromI32(userUpdatePoolRewards.length).toString())
+  );
+
+  // who called the function ?
+  entityUserUpdatePoolReward.sender = event.params.owner; // straight from the event
+  entityUserUpdatePoolReward.from = event.transaction.from;
+
+  // straight from the events
+  entityUserUpdatePoolReward.lpTokenPoolAddress = event.params.lpToken;
+  entityUserUpdatePoolReward.amountTokenPerBlock = event.params.pursePerBlock;
+
+  // For historical data retreival we need timestamp and block number
+  entityUserUpdatePoolReward.timestamp = event.block.timestamp;
+  entityUserUpdatePoolReward.blockNumber = event.block.number;
+
+  entityUserActions.userUpdatePoolRewards = userUpdatePoolRewards.concat([
+    entityUserUpdatePoolReward.id,
+  ]);
+  // userUpdatePoolRewards?.push(entityUserUpdatePoolReward.id);
+  // entityUserActions.userUpdatePoolRewards = userUpdatePoolRewards;
+  entityUserUpdatePoolReward.save();
+  entityUserActions.save();
+}
+
+export function handleWithdraw(event: Withdraw): void {
+  // load UserActions entity
+  let entityUserActions = UserActions.load(
+    event.transaction.from.toHexString()
+  );
+
+  // if UserActions entiry for this transaction caller does not exist, create new.
+  if (entityUserActions === null) {
+    entityUserActions = createUserActions(event.transaction.from);
+  }
+
+  // create new entity for the one in question
+  let userWithdraws = entityUserActions.userWithdraws;
+  let entityUserWithdraw = new UserWithdraw(
+    event.transaction.hash
+      .toHexString()
+      .concat("-")
+      .concat(BigInt.fromI32(userWithdraws.length).toString())
+  );
+
+  // who called the function ?
+  entityUserWithdraw.sender = event.params.user; // straight from the event
+  entityUserWithdraw.from = event.transaction.from;
+
+  // straight from the events
+  entityUserWithdraw.amountWithdraw = event.params.amount;
+
+  // For historical data retreival we need timestamp and block number
+  entityUserWithdraw.timestamp = event.block.timestamp;
+  entityUserWithdraw.blockNumber = event.block.number;
+
+  entityUserActions.userWithdraws = userWithdraws.concat([
+    entityUserWithdraw.id,
+  ]);
+  // userWithdraws?.push(entityUserWithdraw.id);
+  // entityUserActions.userWithdraws = userWithdraws;
+  entityUserWithdraw.save();
+  entityUserActions.save();
+}
+
+/// Issei Kumagai
